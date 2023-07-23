44466e72a2616a1d1516709ff7fdd0f5b669de9e
vovunku <vovunku@mail.ru>
2023-07-23 02:40:12 +0200

    added Ilya as master; fixed percent logic

--- contracts/src/deploy/deploy_plugin.ts
+++ contracts/src/deploy/deploy_plugin.ts
@@ -10,7 +10,7 @@ const deploy: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 
     // We don't use a trusted origin right now to make it easier to test.
     // For production networks it is strongly recommended to set one to avoid potential fee extraction.
-    const trustedOrigin =  hre.network.name === "hardhat" ? "0xcf7ed3acca5a467e9e704c703e8d87f634fb0fc9" : "0x4Ed4a978b67E1a8710c9f00c0e1224151AE26B5F"
+    const trustedOrigin =  hre.network.name === "hardhat" ? "0xcf7ed3acca5a467e9e704c703e8d87f634fb0fc9" : "0xb011a210f04144B5866283D22e9A8616DAD87A92"
     await deploy("SplitPayPlugin", {
         from: deployer,
         args: [trustedOrigin],
